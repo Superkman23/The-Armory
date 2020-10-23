c2fc45c8ddc1637a56f0206fd74f88f4b495240a
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-10-23 17:19:09 -0400

    Added Player movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  Camera _Camera;
+
+  [SerializeField] Transform _TargetPosition;
+  [SerializeField] [Range(0, 1)] float _LerpSpeed;
+
+  private void Awake()
+  {
+    _Camera = GetComponent<Camera>();
+  }
+  private void FixedUpdate()
+  {
+    Vector3 targetPos = Vector3.Lerp(transform.position, _TargetPosition.position, _LerpSpeed);
+    targetPos.z = transform.position.z;
+    transform.position = targetPos;
+  }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  Rigidbody2D _Rigid;
+  [SerializeField] float _MoveSpeed;
+  [SerializeField] float _Acceleration;
+
+
+  private void Awake()
+  {
+    _Rigid = GetComponent<Rigidbody2D>();
+  }
+  private void FixedUpdate()
+  {
+    ApplyMovement();
+  }
+
+
+  private void ApplyMovement()
+  {
+    Vector2 input = GetInput().normalized;
+    Vector2 targetVelocity = input * _MoveSpeed;
+    Vector2 velocityChange = targetVelocity - _Rigid.velocity;
+    float acceleration = _Acceleration * Time.deltaTime;
+    velocityChange.x = Mathf.Clamp(velocityChange.x, -acceleration, acceleration);
+    velocityChange.y = Mathf.Clamp(velocityChange.y, -acceleration, acceleration);
+    _Rigid.velocity += velocityChange;
+  }
+
+
+
+  Vector2 GetInput()
+  {
+    Vector2 input = Vector2.zero;
+    if (Input.GetKey(KeyCode.W))
+    {
+      input.y++;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      input.x--;
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      input.y--;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      input.x++;
+    }
+    return input;
+  }
+
+}
